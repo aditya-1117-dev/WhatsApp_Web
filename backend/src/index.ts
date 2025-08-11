2177d831e3c9135054e560ec97fc135cb490bdd5
adity <sg9850249119@gmail.com>
2025-08-11 17:55:00 +0000

    feat : add the index file for application with the socket connection

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+
+import connectDB from './config/db';
+import webhookRoutes from './routes/webhook.routes';
+import conversationRoutes from './routes/conversations.routes';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+const server = http.createServer(app);
+
+export const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+app.use('/api', webhookRoutes);
+app.use('/api', conversationRoutes);
+
+io.on('connection', (socket) => {
+    console.log('A user connected:');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:');
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
